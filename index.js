977e75f34521072842a714a630a3ae442f904816
elpepe <1941cosf@gmail.com>
2020-02-17 16:49:10 -0500

    prueba0

---
+++ index.js
@@ -0,0 +1,36 @@
+
+// DOC: https://firebase.google.com/docs/firestore/quickstart?authuser=2
+
+        // inicializa la librería de firebase, con los identificadores de nuestros proyectos en firebase.google.com
+        firebase.initializeApp({
+            apiKey: 'AIzaSyAHFyLKWTKe3U_hsSGchsDmcnEtNuSuxNU',
+            authDomain: 'prueba-dd54b.firebaseapp.com',
+            projectId: 'prueba-dd54b'
+        });
+        // en este punto firebase está listo para ser usado
+
+        // se crea un objeto "referencia" para poder darle instrucciones al firestore de nuestro proyecto
+        var db = firebase.firestore();
+
+        writeSomething = () => db.collection("users").add({
+                first: document.getElementById("nombre").value,
+                last: document.getElementById("apellido").value,
+                born: 1990,
+                son: {
+                    first: "odu",
+                    last: "Livinting",
+                    born: 1990
+                }
+            })
+            .then(function(docRef) {
+                console.log("El  Documento con ID: ", docRef.id);
+            })
+            .catch(function(error) {
+                console.error("Error EN EL DOCUMENTO: ", error);     
+            });
+
+        readAll = () => db.collection("users").get().then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    console.log(`${doc.id} => ${doc.data().first}`);
+                });
+            });
